9c7f9cd291f20f8ffdb8c4d98dab5f8eb991b076
Batuhan3737 <batuhanaktas2003@gmail.com>
2020-11-18 17:26:59 +0300

    fibonacci sayıları

---
+++ fibonacci sayıları/fibonacci sayıları/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacci_sayıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciList = new List<int>() {0,1};
+            //fibonacciList.Add();
+
+            int index = 0;
+            int sondeger = 0;
+            int listeson = 0;
+
+            Console.Write("Bitiş Değerini Giriniz: ");
+            sondeger = Convert.ToInt32(Console.ReadLine());
+
+            while (sondeger > index)
+            {
+                listeson = fibonacciList.Count;
+                index = fibonacciList[listeson - 1] + fibonacciList[listeson - 2];
+
+                if (sondeger >= index)
+                {
+                    fibonacciList.Add(index);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < fibonacciList.Count; i++)
+            {
+                Console.WriteLine(fibonacciList[i] + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
